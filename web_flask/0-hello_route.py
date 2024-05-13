a9e328f1c019409f304c0f946ffd5e3e39dac7a5
edwardsila <edwardsila000@gmail.com>
2024-05-13 17:39:35 +0000

    hello flsk

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,15 @@
-#!/usr/bin/python3
-"""
-start flask application
+from flask import Flask
 """
 
-from flask import Flask
+Flask app that runs on 0.0.0.0
+"""
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
-    """ returns Hello HBNB! """
-    return 'Hello HBNB!'
-
+def hello():
+    """ returns Helloe HBNB """
+    return "Hello HBNB!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0' port='5000')
+    app.run(host='0.0.0.0', port=5000)
