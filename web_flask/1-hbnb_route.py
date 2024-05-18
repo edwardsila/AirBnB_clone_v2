863cf259ff9b8a4ea912cb6d38f8c617862ed568
edwardsila <edwardsila000@gmail.com>
2024-05-18 11:09:06 -0700

    hbnb  updated

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,12 +6,13 @@ from flask import Flask
 
 app = flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def home():
-   """
-   displays hbnb
-   """
-   return 'Hello HBNB!'
+    """
+    displays hbnb
+    """
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
@@ -20,6 +21,7 @@ def hbnb():
     returns hbnb
     """
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
