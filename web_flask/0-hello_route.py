dd1ea4dd372741e6c73642f398d2c517038a402a
edwardsila <edwardsila000@gmail.com>
2024-05-18 17:41:21 +0000

    env

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,19 @@
-from flask import Flask
-
+#!/usr/bin/python3
 """
 Flask app that runs on 0.0.0.0
 """
+from flask import Flask
+
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    """ returns Helloe HBNB """
-    return "Hello HBNB!"
-
+def home():
+    """
+    home hbnb!
+    """
+    return 'Hello HBNB!'
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
